ae26f98c929794c5c7c7d5d2a3f5c105c2ad286a
Ian McCormack <icmccorm@andrew.cmu.edu>
2022-11-01 22:10:14 -0400

    Finished script to check for explicit foreign function declarations.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,101 @@
 use std::env;
-use syn::visit::{self, Visit};
-use syn::{File, ItemFn};
 use std::fs::read_to_string;
+use syn::spanned::Spanned;
+use syn::token::{Colon, Default};
+use syn::visit::{self, Visit};
+use syn::Type;
+use syn::{ExprCall, ExprMethodCall, File, ForeignItemFn, PathSegment, ReturnType};
+use walkdir::WalkDir;
 
 fn read_file_string(filepath: &str) -> Result<String, Box<dyn std::error::Error>> {
     let data = read_to_string(filepath)?;
     Ok(data)
 }
 
+fn string_of_type(t: &Type) -> String {
+    match t {
+        Type::Path(tp) => {
+            let segment_names: Vec<String> = tp
+                .path
+                .segments
+                .iter()
+                .map(|ps| ps.ident.to_string())
+                .collect();
+            segment_names.join("::")
+        }
+        Type::Ptr(inner) => String::from("*") + &string_of_type(&*inner.elem),
+        Type::Reference(inner) => String::from("&") + &string_of_type(&*inner.elem),
+        Type::Slice(_) => String::from("Slice"),
+        Type::TraitObject(_) => String::from("TraitObject"),
+        Type::Tuple(_) => String::from("Tuple"),
+        Type::Verbatim(_) => String::from("Verbatim"),
+        _ => String::from("N/A"),
+    }
+}
 
-struct FnVisitor;
+struct ForeignDecl {
+    id: usize,
+    name: String,
+}
+struct FnVisitor<'a> {
+    id_counter: usize,
+    decls: &'a mut Vec<ForeignDecl>,
+}
 
-impl<'ast> Visit<'ast> for FnVisitor {
-    fn visit_item_fn(&mut self, node: &'ast ItemFn) {
-        println!("Function with name={}", node.sig.ident);
+struct FnArg {
+    function_id: usize,
+    reference_type: bool,
+}
 
-        // Delegate to the default impl to visit any nested functions.
-        visit::visit_item_fn(self, node);
+impl<'ast> Visit<'ast> for FnVisitor<'_> {
+    fn visit_foreign_item_fn(&mut self, node: &'ast ForeignItemFn) {
+        let repr = ForeignDecl {
+            id: self.id_counter,
+            name: node.sig.ident.to_string()
+        };
+        self.id_counter += 1;
+        self.decls.push(repr);
+        
+        visit::visit_foreign_item_fn(self, node);
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    for arg in args {
-        match read_file_string(&arg) {
-            Ok(code) => {
-                let syntax_tree: File = syn::parse_str(&code).unwrap();
-                FnVisitor.visit_file(&syntax_tree);
+    match args.get(1) {
+        Some(arg) => {
+            let mut visitor = FnVisitor {
+                id_counter: 0,
+                decls: & mut Vec::<ForeignDecl>::new(),
+            };
+            for entry in WalkDir::new(arg)
+                .follow_links(true)
+                .into_iter()
+                .filter_map(|e| e.ok())
+            {
+                let f_name = entry.file_name().to_string_lossy();
+                if f_name.ends_with(".rs") {
+                    match entry.path().to_str() {
+                        Some(resolved_path) => match read_file_string(&resolved_path) {
+                            Ok(code) => {
+                                let syntax_tree: File = syn::parse_str(&code).unwrap();
+                                visitor.visit_file(&syntax_tree);
+                            }
+                            Err(_) => {
+                            }
+                        },
+                        None => {}
+                    }
+                }
             }
-            Err(_) => {
-                println!("Error: unable to parse file.")
+            if(visitor.decls.len() > 0) {
+                std::process::exit(0);
+            }else{
+                std::process::exit(1);
             }
+        }
+        None => {
+            panic!("Usage: findffi [dir]");
         }
     }
 }
--- verify.sh
+++ verify.sh
@@ -2,19 +2,28 @@
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
+cargo build 1> /dev/null
 rm -f ./data/verified.csv > /dev/null
-rm -f ./data/failed.csv > /dev/null
 rm -rf ./test > /dev/null
 while IFS=, read crate_id name updated_at created_at downloads repository version; 
 do 
-    cargo-download -x "$name==$version" --output ./test 1> /dev/null
-    if (cd test && cargo check --lib 1> /dev/null); then
-        echo "${GREEN}PASSED${NC} $name\n" 
-        echo "$crate_id,$name" >> ./data/verified.csv
+    if (cargo-download -x "$name==$version" --output ./test 1> /dev/null); then 
+        if (./target/debug/cargo-ffi ./test); then 
+            if (cd test && cargo check --lib 1> /dev/null); then
+                echo "${GREEN}PASSED${NC} $name\n" 
+                echo "$crate_id,$name,passed" >> ./data/verified.csv
+            else
+                echo "${RED}CHECK FAILED${NC} $name\n"
+                echo "$crate_id,$name,check-failed" >> ./data/verified.csv
+            fi
+        else
+            echo "${RED}NO ABI${NC} $name\n"
+            echo "$crate_id,$name,noabi" >> ./data/verified.csv
+        fi
     else
-        echo "${RED}FAILED${NC} $name\n"
-        echo "$crate_id,$name" >> ./data/failed.csv
+        echo "${RED}DOWNLOAD FAILED${NC} $name\n"
+        echo "$crate_id,$name,download-failed" >> ./data/verified.csv
     fi
     rm -rf ./test
-done < top20.csv
+done < ./data/crates.csv
 echo "${GREEN}FINISHED!${NC} $name\n" 
